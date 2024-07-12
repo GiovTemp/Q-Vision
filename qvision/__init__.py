4832a1ecd3d90ed1567c90085a28bb41b28ffd96
Giovanni Tempesta <giovannitempestainfo@gmail.com>
2024-07-12 20:44:55 +0200

    add dataset preprocessing functions

--- qvision/__init__.py
+++ qvision/__init__.py
@@ -2,3 +2,4 @@
 
 from .neuron import neuron, spatial_loss_derivative, Fourier_loss_derivative, update_rule, optimization
 from .utils import sig, sigPrime, loss, accuracy
+from .preprocessing import rgb2gray, convert_to_float, convert_and_normalize, calculate_amplitudes
