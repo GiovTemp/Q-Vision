060c7647e671c438c5c31f7891d54cda3ae9c5f1
Mich-31 <83719639+Mich-31@users.noreply.github.com>
2024-07-15 14:34:30 +0200

    Update training.py

--- qvision/training.py
+++ qvision/training.py
@@ -1,12 +1,12 @@
 # Q-Vision/qvision/training.py
 
 import numpy as np
-from .neuron import neuron, optimization_rmsprop, spatial_loss_derivative
+from .neuron import neuron, optimization, spatial_loss_derivative
 from .utils import loss, accuracy
 
 def train(weights, bias, trainImgs, trainLabels, testImgs, testLabels, num_epochs, lr_weights, lr_bias, num_shots):
     """Train the model using the optimization function from neuron.py"""
-    weights, bias, loss_history, test_loss_history, accuracy_history, test_accuracy_history = optimization_rmsprop(
+    weights, bias, loss_history, test_loss_history, accuracy_history, test_accuracy_history = optimization(
         spatial_loss_derivative, weights, bias, trainLabels, testLabels, trainImgs, testImgs,
         num_epochs, lr_weights, lr_bias, num_shots
     )
