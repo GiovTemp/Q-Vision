4ab8fe7ce1c2e404a1f14d58a7c61647a249e875
Mich-31 <83719639+Mich-31@users.noreply.github.com>
2024-07-15 14:34:51 +0200

    Update __init__.py

--- qvision/__init__.py
+++ qvision/__init__.py
@@ -1,6 +1,6 @@
 # Q-Vision/qvision/__init__.py
 
-from .neuron import neuron, spatial_loss_derivative, Fourier_loss_derivative, update_rule_rmsprop, optimization_rmsprop
+from .neuron import neuron, spatial_loss_derivative, Fourier_loss_derivative, update_rule, optimization
 from .utils import sig, sigPrime, loss, accuracy
 from .preprocessing import rgb2gray, convert_to_float, convert_and_normalize, calculate_amplitudes
 from .training import train
