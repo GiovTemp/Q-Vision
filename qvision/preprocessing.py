9104e039b69df77413dbbd26958275d591ca69b6
Giovanni Tempesta <giovannitempestainfo@gmail.com>
2024-10-28 13:29:41 +0100

    Updated function

--- qvision/preprocessing.py
+++ qvision/preprocessing.py
@@ -1,15 +1,14 @@
-import numpy as np
 
+
+import numpy as np
 
 def rgb2gray(rgb):
     """Convert an RGB digital image to grayscale."""
-    return np.dot(rgb[..., :3], [0.2989, 0.5870, 0.1140])
-
+    return np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140])
 
 def convert_to_float(images, labels):
     """Convert images and labels to float64."""
     return images.astype(np.float64), labels.astype(np.float64)
-
 
 def convert_and_normalize(images):
     """Convert images to grayscale and normalize them."""
@@ -20,7 +19,6 @@ def convert_and_normalize(images):
             img_gray = img
         images[idx, :, :, 0] = img_gray / np.sum(img_gray)
     return images
-
 
 def calculate_amplitudes(images):
     """Calculate amplitudes of the images."""
