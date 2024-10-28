05a593af6f99ec690eacfbc0f441a63af405cb6c
Giovanni Tempesta <giovannitempestainfo@gmail.com>
2024-10-28 17:39:34 +0100

    updated preprocessing

--- qvision/preprocessing.py
+++ qvision/preprocessing.py
@@ -1,31 +1,28 @@
-
-
-import numpy as np
+import cupy as cp
 
 def rgb2gray(rgb):
     """Convert an RGB digital image to grayscale."""
-    return np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140])
+    return cp.dot(rgb[..., :3], [0.2989, 0.5870, 0.1140])
 
 def convert_to_float(images, labels):
     """Convert images and labels to float64."""
-    return images.astype(np.float64), labels.astype(np.float64)
+    return images.astype(cp.float64), labels.astype(cp.float64)
 
 def convert_and_normalize(images):
     """Convert images to grayscale and normalize them."""
-
-    for idx, img in enumerate(images):
-        if len(images.shape) != 3:
+    for idx in range(images.shape[0]):  # Cambiato per lavorare con gli indici di CuPy
+        img = images[idx]
+        if len(images.shape) == 4:  # Controllo se le immagini hanno una dimensione di 4 (batch con canale)
             img_gray = rgb2gray(img)
-            images[idx, :, :, 0] = img_gray / np.sum(img_gray)
+            images[idx, :, :, 0] = img_gray / cp.sum(img_gray)  # Usa cp.sum invece di np.sum
         else:
             img_gray = img
-            images[idx, :, :] = img_gray / np.sum(img_gray)
-
+            images[idx, :, :] = img_gray / cp.sum(img_gray)  # Usa cp.sum invece di np.sum
     return images
 
 def calculate_amplitudes(images):
     """Calculate amplitudes of the images."""
-    if len(images.shape) != 3:
-        return np.sqrt(images[:, :, :, 0])
+    if len(images.shape) == 4:  # Controllo per batch con canale
+        return cp.sqrt(images[:, :, :, 0])  # Usa cp.sqrt invece di np.sqrt
     else:
-        return np.sqrt(images[:, :, :])
+        return cp.sqrt(images[:, :, :])  # Usa cp.sqrt invece di np.sqrt
